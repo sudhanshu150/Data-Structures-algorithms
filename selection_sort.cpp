5ef67536c2bccc9415cce7e026df5fad785f95a8
SUDHANSHU <42195119+sudhanshu150@users.noreply.github.com>
2021-05-25 10:03:20 +0530

    selection sort algorithm

---
+++ selection_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,arr[100],temp;
+    cout<<"enter the size of array:";
+    cin>>n;
+    for(int i = 0; i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    cout<<"array after sorting:";
+    for(int i = 0; i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+
+    return 0;
+}
